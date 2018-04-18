3d887492f893fc1a1f37df98348bb21d7729ebd3
John Gilligan <jgilligan@ucvts.tec.nj.us>
2018-04-19 02:35:34 +0830

    update js,css,character

--- amar.js
+++ amar.js
@@ -1,2 +1,28 @@
+var character;
+
+function knightStart(){
+  character = "knight";
+}
+
+function sorcererStart(){
+  character =  "sorcerer";
+}
+
+function archerStart(){
+  character =  "archer";
+}
+
 function start(){
+  var hidden = document.getElementById('characterSelect');
+  if (hidden.style.display === "none") {
+        hidden.style.display = "block";
+    } else {
+        hidden.style.display = "none";
+    }
+  var show = document.getElementById('home');
+  if (show.style.display === "none") {
+        show.style.display = "block";
+    } else {
+        show.style.display = "none";
+    }
 }
