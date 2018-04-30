d49935ad03f81edb1788b7e7859954b0e0cd1ad5
righty76 <37844526+righty76@users.noreply.github.com>
2018-04-30 13:22:00 -0400

    Update amar.js

--- amar.js
+++ amar.js
@@ -1,6 +1,7 @@
 var character;
 var health;
-var enemyHealth;
+var enemyHealth = 100;
+var balance;
 
 function start(person){
   character = person;
@@ -33,49 +34,89 @@ function explore(){
     } else {
         exploreShow.style.display = "none";
   }
-  if(character = knight){
-    battle(character, 200);
-    console.log("explore",character);
-  } else if(character = archer){
-    battle(character, 150);
-    console.log("explore", character);
-  } else if(character = sorcerer){
-    battle(character, 100);
-    console.log("explore", character);
+  healthRestore();
+  enemyHealthRestore('minion');
+}
+
+function explore2(){
+  var show = document.getElementById('home');
+  if (show.style.display === "none") {
+        show.style.display = "block";
+    } else {
+        show.style.display = "none";
   }
 }
 
-function attack(enemy){
-
-  if(character = knight){
-    battle(character, 200);
+function attack(){
+  console.log('character: ' + character);
+  if(character == 'knight'){
+    battle(character);
     console.log("explore",character);
-  } else if(character = archer){
-    battle(character, 150);
+  } else if(character == 'archer'){
+    battle(character);
     console.log("explore", character);
-  } else if(character = sorcerer){
-    battle(character, 100);
+  } else if(character == 'sorcerer'){
+    battle(character);
     console.log("explore", character);
   }
 }
+ function enemyHealthRestore(enemy){
+   if (enemy = 'minion'){
+     enemyHealth = 100;
+   } else if (enemy = 'warrior'){
+     enemyHealth = 200;
+   }
+ }
 
 function healthRestore(){
-  if(character = knight){
+  if(character == 'knight'){
     health = 200;
-  } else if(character = archer){
+  } else if(character == 'archer'){
     health = 150;
-  } else if(character = sorcerer){
+  } else if(character == 'sorcerer'){
     health = 100;
   }
 }
 
-function battle(character, health){
-  if(character = knight){
+function battle(character){
+  if(character == 'knight'){
     enemyHealth = enemyHealth-25;
-  } else if(character = archer){
+    health = health-20;
+    checkWin(character);
+    console.log(enemyHealth, health, character);
+  } else if(character == 'archer'){
     enemyHealth = enemyHealth-50;
-  } else if(character = sorcerer){
-    enemyHealth = 100;
+    health = health-20;
+    checkWin(character);
+    console.log(enemyHealth, health, character);
+  } else if(character == 'sorcerer'){
+    enemyHealth = enemyHealth-75;
+    health = health-20;
+    checkWin(character);
+    console.log(enemyHealth, health, character);
+  }
+}
+
+function checkWin(character){
+  if (health == 0){
+    alert("You have been defeated!");
+    enemyHealthRestore('minion');
+  } else if (enemyHealth == 0){
+    alert("Congratulations! The enemy Amarian has been defeated!");
+    healthRestore();
+    enemyHealthRestore('warrior');
+    var victoryShow = document.getElementById('victory1');
+    if (victoryShow.style.display === "none") {
+          victoryShow.style.display = "block";
+      } else {
+          victoryShow.style.display = "none";
+    }
+    var exploreShow = document.getElementById('explorePage');
+    if (exploreShow.style.display === "none") {
+          exploreShow.style.display = "block";
+      } else {
+          exploreShow.style.display = "none";
+    }
   }
 }
 
